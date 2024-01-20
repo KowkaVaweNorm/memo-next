cf2b881687dd58652878d448d2b817d3c412d680
KowkaVN <kowka25122000@gmail.com>
2024-01-21 00:02:28 +0300

    Add btn for next page
    Added a button to go to the next page

--- src/widgets/Card/ui/Card/Card.tsx
+++ src/widgets/Card/ui/Card/Card.tsx
@@ -1,11 +1,12 @@
 "use client"
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import { classNames } from '@/shared/lib/ClassNames/ClassNames';
 import { CardQuestions } from '../CardQuestion/CardQuestions';
 import cls from './Card.module.scss';
 import { CardSchema } from '@/entities/Card';
 import { CardAnswers } from '../CardAnswers/CardAnswers';
-
+import ArrowRight from '@/shared/assets/icons/arrow-right.svg'
+import Image from 'next/image';
 interface ICardProps {
   card: CardSchema
   className?: string;
@@ -16,6 +17,13 @@ export const Card = memo((props: ICardProps): JSX.Element => {
     card,
     className
   } = props;
+  const [allowNextCard, setAllowNextCard] = useState<boolean>(false)
+  
+  const handleAnswerClick = useCallback(() => {
+    console.log('click');
+
+    setAllowNextCard(true)
+  }, [])
 
   return (
     <div className={classNames(cls.card ?? '', {}, [className])}>
@@ -23,7 +31,20 @@ export const Card = memo((props: ICardProps): JSX.Element => {
         className={cls.question}
         question={card.question}
       />
-      <CardAnswers className={cls.answers} answers={card.answers} />
+       <CardAnswers
+        className={cls.answers}
+        answers={card.answers}
+        onSelectAnswer={handleAnswerClick} />
+      <button className={
+        classNames(cls.next_page_btn, {
+          [cls.active]: allowNextCard
+        }, [])}>
+        <Image
+          src={ArrowRight}
+          alt=''
+          className={
+            classNames(cls.arrow_right, {}, [])} />
+      </button>
     </div>
   );
 });
